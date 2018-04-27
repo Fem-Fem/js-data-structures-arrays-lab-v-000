484c6e495ed82deddfb3cbddaadb59bfe813e449
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-27 04:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyPrependDriver(name) {
 }
 
 function PrependDriver(name) {
-  return [name, ...drivers.slice(1, 3)]
+  // return [name, ...drivers.slice(1, 3)]
 }
 
 function destructivelyRemoveLastDriver() {
