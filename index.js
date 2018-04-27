668e6b88dfb2d2cd74f289b41e0728c3c5578405
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-27 04:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendDriver(name) {
+function destructivelyAppendDriver() {
   drivers.push("Ralph")
 }
 
-function appendDriver(name) {
+function appendDriver() {
   return [...drivers.slice(0, 3), 'Broom']
 }
 
-function destructivelyPrependDriver(name) {
+function destructivelyPrependDriver() {
   drivers.unshift("Garfield")
   // drivers
 }
 
-function PrependDriver(name) {
+function PrependDriver() {
   drivers.unshift("Ralph")
 }
+
+function destructivelyRemoveLastDriver()
